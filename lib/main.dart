de1d73a0f2923fdf7e0fae8682b7f0db057e5812
Tizio GIRBAL <tiziog27@gmail.com>
2025-03-25 17:32:08 +0100

    Sauvegarde des modifications avant changement de branche

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const ChessApp());
+  runApp(const MyApp());
 }
 
-class ChessApp extends StatelessWidget {
-  const ChessApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -26,27 +26,32 @@ class ChessScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _buildPlayerName("Joueur 1"),
+          _buildPlayerName("Joueur 1", true),
           Expanded(
             child: Center(
               child: Image.asset(
-                'assets/echiquier.jpg',
-                width: MediaQuery.of(context).size.width * 0.9,
+                'assets/echiquier_crop.jpg',
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.cover,
               ),
             ),
           ),
-          _buildPlayerName("Joueur 2"),
+          _buildPlayerName("Joueur 2", false),
         ],
       ),
     );
   }
 
-  Widget _buildPlayerName(String name) {
+  Widget _buildPlayerName(String name, bool flip) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Text(
-        name,
-        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      child: Transform(
+        alignment: Alignment.center,
+        transform: flip ? Matrix4.rotationX(3.1416) * Matrix4.rotationY(3.1416) : Matrix4.identity(),
+        child: Text(
+          name,
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
